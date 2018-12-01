aa19a481d323cb4b745d2c761953dfe6464b7ea9
Neshametha <nwhit028@fiu.edu>
2018-12-01 18:24:43 -0500

    Fix buttons (for realsies?)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { PropTypes } from 'react';
 import { Button } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
+import './props.js';
 
 
 class App extends React.Component {
+
   render() {
+
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -26,6 +30,27 @@ class App extends React.Component {
       </div>
     );
   }
+}
+
+Button.propTypes = {
+  active: PropTypes.bool,
+  block: PropTypes.bool,
+  color: PropTypes.string, // default: 'secondary'
+  disabled: PropTypes.bool,
+
+  // Pass in a Component to override default button element
+  // example: react-router Link
+  // default: 'button'
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+
+  // ref will only get you a reference to the Button component, use innerRef to get a reference to the DOM element (for things like focus management).
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+
+  onClick: PropTypes.func,
+  size: PropTypes.string,
+
+  // use close prop for BS4 close icon utility
+  close: PropTypes.bool,
 }
 
 export default App;
